34772861abdeea14172e28ec2c9a62fb86204ecc
nooboftime <tamara_walling@yahoo.com>
2024-10-29 12:40:06 -0500

    lint errors

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let value of array) {
-    const index = array.indexOf(value);
-    array[index] = appendString + value;
+  const newArray = [];
+    for (const value of array) {
+      newArray.push(appendString + value);
   }
 
-  return array;
+  return newArray;
 }
