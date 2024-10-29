699e071daa42b868fff6dd92e61e6767a2d692d6
nooboftime <tamara_walling@yahoo.com>
2024-10-29 11:28:44 -0500

    removing self

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,1 +1,9 @@
-sd
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+}
+
