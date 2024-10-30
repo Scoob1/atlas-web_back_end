ab765220d0e9c46beafdc49faa8617ca750d6e89
nooboftime <tamara_walling@yahoo.com>
2024-10-30 09:56:16 -0500

    imported file5, implemented a class skyhighbuilding

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,1 +1,19 @@
-dfh
+import Building from './5-buildign';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  // Getter for floors
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Floors must be a number');
+    }
+    this._floors = value;
+  }
