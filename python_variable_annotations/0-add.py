f1e7d6039f14e0061cabca34b9d3cd9157fcb2f1
nooboftime <tamara_walling@yahoo.com>
2025-01-07 13:17:14 -0600

    actually two blank lines were nedded before the add function

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -4,6 +4,7 @@ type-annotated that takes a float a and b and returns their sum as a float.
 
 """
 
+
 def add(a: float, b: float) -> float:
     """
     Adds two float numbers and returns their sum.
@@ -16,4 +17,3 @@ def add(a: float, b: float) -> float:
         float: The sum of the two num.
     """
     return a + b
-
