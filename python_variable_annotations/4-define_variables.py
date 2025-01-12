88bd41c9fd9188303a5be23f3f3a3ca26619f95a
nooboftime <tamara_walling@yahoo.com>
2025-01-12 14:44:30 -0600

    annotate the following variables with the specified values

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Defines and annotates var with specified values and types.
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
