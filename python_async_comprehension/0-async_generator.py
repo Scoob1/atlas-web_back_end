926788d589d0df68062c4eacabeba081ac39e07d
nooboftime <tamara_walling@yahoo.com>
2025-01-12 16:45:29 -0600

    impoting generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,7 +2,7 @@
 """A coroutine that yields a random number between 0 and 10, 10 times."""
 import asyncio
 import random
-
+from typing import Generator
 
 async def async_generator() -> Generator[float, None, None]:
     """A coroutine that yields a random number between 0 and 10, 10 times."""
