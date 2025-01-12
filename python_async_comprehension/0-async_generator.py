a554a330180dec35f2ef80ae758a76cebe3070fe
nooboftime <tamara_walling@yahoo.com>
2025-01-12 16:46:27 -0600

    pep8 two blank lines

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,6 +4,7 @@ import asyncio
 import random
 from typing import Generator
 
+
 async def async_generator() -> Generator[float, None, None]:
     """A coroutine that yields a random number between 0 and 10, 10 times."""
     for _ in range(10):
