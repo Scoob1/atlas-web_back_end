4b4364532c7c665f195ad78d47853bd9ecd8ab28
nooboftime <tamara_walling@yahoo.com>
2025-01-12 16:43:44 -0600

    async_generator is annotated

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """A coroutine that yields a random number between 0 and 10, 10 times."""
     for _ in range(10):
         await asyncio.sleep(1)
