fbd4e311654e8a2126af34fa30de0a1f8552de11
nooboftime <tamara_walling@yahoo.com>
2025-01-12 18:18:45 -0600

    added parenthesis

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -25,4 +25,5 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     delays = await asyncio.gather(
         *(wait_random_module.wait_random(max_delay) for _ in range(n))
+)
     return sorted(delays)
