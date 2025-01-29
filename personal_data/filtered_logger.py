27c2984f09544b71a3a5a53ea70bfe28177154af
nooboftime <tamara_walling@yahoo.com>
2025-01-29 12:36:36 -0600

    filterr datum

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,1 +1,19 @@
-hg
+#!/usr/bin/env python3
+import re
+
+def filter_datum(fields, redaction, message, separator):
+    """
+    Obfuscates specified fields in the log message.
+
+    Arguments:
+    fields: list of fields to obfuscate.
+    redaction: the string to replace the field values.
+    message: the log message to modify.
+    separator: character separating the fields.
+    
+    Returns:
+    The modified log message with obfuscated fields.
+    """
+    for field in fields:
+        message = re.sub(f'{field}=[^;]+', f'{field}={redaction}', message)
+    return message
