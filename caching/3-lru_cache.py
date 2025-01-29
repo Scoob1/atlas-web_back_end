b9c8b0849b245194d8778fbfe6d05f20416bf74c
nooboftime <tamara_walling@yahoo.com>
2025-01-29 12:04:24 -0600

    inherits from basecahcing

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -1,1 +1,24 @@
-dfg
+#!/usr/bin/env python3
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                del self.cache_data[key]
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discarded_key, _ = self.cache_data.popitem(last=False)
+                print(f"DISCARD: {discarded_key}")
+            self.cache_data[key] = item
+
+    def get(self, key):
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key)
+            return self.cache_data[key]
+        return None
