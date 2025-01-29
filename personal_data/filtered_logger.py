af90ad061c52fc7326df1ba9da1da6919b8222cb
nooboftime <tamara_walling@yahoo.com>
2025-01-29 12:42:29 -0600

    redactoin str

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,7 +3,7 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """
     Obfuscates specified fields in the log message.
 
