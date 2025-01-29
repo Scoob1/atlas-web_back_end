57038b3cbe9807815baf5a56580f6937c5a686c2
nooboftime <tamara_walling@yahoo.com>
2025-01-29 12:09:15 -0600

    doc strings

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -4,11 +4,23 @@ from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
+    """
+    LRUCache class that implements LRU caching.
+    """
+
     def __init__(self):
+        """
+        Initializes the LRUCache instance.
+        """
         super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
+        """
+        Adds an item to the cache.
+        
+        If the cache exceeds the limit, removes the least recently used item.
+        """
         if key is not None and item is not None:
             if key in self.cache_data:
                 del self.cache_data[key]
@@ -18,6 +30,9 @@ class LRUCache(BaseCaching):
             self.cache_data[key] = item
 
     def get(self, key):
+        """
+        Returns the item associated with the key.
+        """
         if key is not None and key in self.cache_data:
             self.cache_data.move_to_end(key)
             return self.cache_data[key]
