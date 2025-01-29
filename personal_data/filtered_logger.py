ad77407e946943a162158a77c14cc7927d74f91d
nooboftime <tamara_walling@yahoo.com>
2025-01-29 12:48:39 -0600

    trailing whitespace and fixed lines being too long

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,7 +3,8 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str],
+                 redaction: str, message: str, separator: str) -> str:
     """
     Obfuscates specified fields in the log message.
 
@@ -12,10 +13,11 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
     redaction: the string to replace the field values.
     message: the log message to modify.
     separator: character separating the fields.
-    
+
     Returns:
     The modified log message with obfuscated fields.
     """
     for field in fields:
-        message = re.sub(f'{field}=[^{separator}]+', f'{field}={redaction}', message)
+        message = re.sub(f'{field}=[^{separator}]+',
+                         f'{field}={redaction}', message)
     return message
