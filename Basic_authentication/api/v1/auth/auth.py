8d03e088593ffd2a6eef4fffcb6dd252e83ad812
nooboftime <tamara_walling@yahoo.com>
2025-01-30 01:00:39 -0600

    paths include exclude true false

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,19 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Determines if authentication is required"""
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+          return True
+    
+    
+        if not path.endswith('/'):
+            path += '/'
+
+
+        if path in excluded_paths:
+            return False
+    
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Retrieves the authorization header"""
