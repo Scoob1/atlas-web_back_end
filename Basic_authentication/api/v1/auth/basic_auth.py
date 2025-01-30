31fb3eb1574f2aca1c85f333b6cf9209a293c688
nooboftime <tamara_walling@yahoo.com>
2025-01-30 17:57:28 -0600

    circular imports

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,9 @@
 # avoiding circular imports
 class BasicAuth(Auth):
     """Basic authentication class inheriting from Auth"""
-    pass
+
+
+    def __init__(self):
+        # Import Auth here, inside the constructor, to avoid circular imports
+        from api.v1.auth.auth import Auth
+        self.auth = Auth()
