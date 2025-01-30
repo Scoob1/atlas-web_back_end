2f71900fa214b83a16a40ab7ce742392fb97bd48
nooboftime <tamara_walling@yahoo.com>
2025-01-30 16:52:42 -0600

    before request

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -14,6 +14,31 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
+auth = None
+AUTH_TYPE = getenv("AUTH_TYPE")
+
+if AUTH_TYPE == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+@app.before_request
+def before_request():
+    """ Filter requests before processing """
+    if auth is None:
+        return
+    
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+    
+    if auth.authorization_header(request) is None:
+        abort(401, description="Unauthorized")
+    
+    if auth.current_user(request) is None:
+        abort(403, description="Forbidden")
+
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
