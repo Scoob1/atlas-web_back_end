cb50956310b564d3cd2c0fa8336fd4d4c9c066c8
nooboftime <tamara_walling@yahoo.com>
2025-01-29 20:59:18 -0600

    validate that the passwords match

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -15,3 +15,12 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode(), salt)
     return hashed_password
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validate a password against a given hashed password.
+    :param hashed_password: The hashed password.
+    :param password: The plain text password to verify.
+    :return: True if the password matches, False otherwise.
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
