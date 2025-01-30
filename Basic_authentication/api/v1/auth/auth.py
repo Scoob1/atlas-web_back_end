2db112680bc97974cd556f1ea0cc03d9dfd7a812
nooboftime <tamara_walling@yahoo.com>
2025-01-30 17:17:23 -0600

    emepty basicauth, auth type

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -3,6 +3,12 @@
 """
 from flask import request
 from typing import List, TypeVar
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Basic authentication class inheriting from Auth"""
+    pass
 
 
 class Auth:
