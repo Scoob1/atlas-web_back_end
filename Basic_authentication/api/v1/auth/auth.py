34fc07e9814569e9e557a063b04d7aa4aa464394
nooboftime <tamara_walling@yahoo.com>
2025-01-30 18:01:35 -0600

    still circularrrrr

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -3,7 +3,6 @@
 """
 from flask import request
 from typing import List, TypeVar
-from api.v1.auth.auth import Auth
 
 
 class Auth:
--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,12 +2,19 @@
 """ Basic Authentication module
 """
 
-# avoiding circular imports
-class BasicAuth(Auth):
+class BasicAuth:
     """Basic authentication class inheriting from Auth"""
 
-
     def __init__(self):
-        # Import Auth here, inside the constructor, to avoid circular imports
         from api.v1.auth.auth import Auth
         self.auth = Auth()
+
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        return self.auth.require_auth(path, excluded_paths)
+
+    def authorization_header(self, request=None) -> str:
+        return self.auth.authorization_header(request)
+
+    def current_user(self, request=None):
+        return self.auth.current_user(request)
