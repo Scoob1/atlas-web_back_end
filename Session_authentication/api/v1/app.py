fa9ca48f3ff5b11e87adf27aaf78b285d292e7f3
nooboftime <tamara_walling@yahoo.com>
2025-01-30 22:24:11 -0600

    editing users for authenicated user

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -45,6 +45,8 @@ def before_request():
     if auth.authorization_header(request) is None:
         abort(401, description="Unauthorized")
 
+        request.current_user = auth.current_user(request)
+
     if auth.current_user(request) is None:
         abort(403, description="Forbidden")
 
