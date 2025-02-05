ccc86e490d92049ec64377d497208e21918c8e5b
nooboftime <tamara_walling@yahoo.com>
2025-02-05 16:37:50 -0600

    license

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -45,5 +45,18 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_repos_url.assert_called_once()
         mock_get_json.assert_called_once_with("https://api.github.com/orgs/test_org/repos")
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        client = GithubOrgClient("test_org")
+        with patch.object(client,
+                          'repos', new_callable=PropertyMock) as mock_repos:
+            mock_repos.return_value = [repo]
+            result = client.has_license(license_key)
+            self.assertEqual(result, expected)
+
+
 if __name__ == "__main__":
     unittest.main()
