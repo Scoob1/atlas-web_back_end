d7970d5a100b653f69c9bfc230e4137d83331a67
nooboftime <tamara_walling@yahoo.com>
2025-02-18 14:15:37 -0600

    MAIN STYLE NOW

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- Lists all bands with Glam rock as their main style rank by longevity
-SELECT band_name, 
-       IFNULL(split - formed, YEAR(CURDATE()) - formed) AS lifespan
+SELECT band_name,
+        TIMESTAMPDIFF(YEAR, formed, IFNULL(split, CURRENT_DATE)) AS lifespan
 FROM metal_bands
 WHERE style = 'Glam rock'
 ORDER BY lifespan DESC;
