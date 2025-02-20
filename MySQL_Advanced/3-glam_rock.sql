fdf626dcd3cdffa057732fcb516cc37610c711ea
nooboftime <tamara_walling@yahoo.com>
2025-02-19 19:09:17 -0600

    making the year 2024

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- Lists all bands with Glam rock as their main style rank by longevity
 SELECT band_name AS 'band_name',
-        TIMESTAMPDIFF(YEAR, formed, IFNULL(split, CURRENT_DATE)) AS lifespan
+        TIMESTAMPDIFF(YEAR, formed, IFNULL(split, '2024-01-01')) AS lifespan
 FROM metal_bands
-WHERE style = 'Glam rock'
+WHERE LOWER(style) = 'Glam rock'
 ORDER BY lifespan DESC;
