ee5e611b7352e308d1e9ed3f0e2221d89498ed55
nooboftime <tamara_walling@yahoo.com>
2025-02-19 19:11:33 -0600

    capitalizing both

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -2,5 +2,5 @@
 SELECT band_name AS 'band_name',
         TIMESTAMPDIFF(YEAR, formed, IFNULL(split, '2024-01-01')) AS lifespan
 FROM metal_bands
-WHERE LOWER(style) = 'glam rock'
+WHERE LOWER(style) = 'Glam Rock'
 ORDER BY lifespan DESC;
